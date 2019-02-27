4760f1efe3287b146a90005cb53ad73d8b8b60a3
Eric Storey <estorey@gmail.com>
2019-02-27 19:49:40 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -3,5 +3,5 @@ def introduction(name)
 end
 
 def introduction_with_language(name, lang)
-  puts "hi, my name is #{name} and I am learning to program #{lang}."
+  puts "Hi, my name is #{name} and I am learning to program #{lang}."
 end
